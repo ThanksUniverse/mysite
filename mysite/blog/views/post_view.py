4eac9a73c826ad846b9042d0d57516169cdaf620
Alt0b11 <thanksuniverse333@outlook.com>
2024-08-08 17:35:06 -0300

    feat: added viewPage for Posts

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
 from django.views import generic
+from blog.models import Post
 
-class PostView(generic.View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello, World!')
+class PostView(generic.ListView):
+    queryset = Post.objects.filter(status=0).order_by('-created_on')
+    template_name = "index.html"
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
